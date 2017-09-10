bc266512d82961ee4ce50a0cb59714ef791038fa
Björn Hegstam <bjorn.hegstam@gmail.com>
2017-09-10 16:42:05 +0200

    Can set port via environment

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,10 +5,14 @@ import spark.Service;
 import util.JsonResponseTransformer;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static spark.Service.ignite;
 
 public class Application {
+
+    private static final int DEFAULT_PORT = 4567;
+
     public static void main(String[] args) {
         new Application().init();
     }
@@ -16,9 +20,16 @@ public class Application {
     private void init() {
         Service http = ignite();
 
-        http.port(4567);
+        http.port(getPort());
 
         http.post("/measurement", "application/json", this::postMeasurement, new JsonResponseTransformer());
+    }
+
+    private int getPort() {
+        return Optional
+                .ofNullable(System.getenv("PORT"))
+                .map(Integer::parseInt)
+                .orElse(DEFAULT_PORT);
     }
 
     private Object postMeasurement(Request request, Response response) {
